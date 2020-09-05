ce3c3c9f57ee2fc72c5ae78c1edf1e91b551be61
Jonny Lightheart <jonnylightheart@gmail.com>
2020-09-05 03:28:07 -0600

    second page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEL = document.querySelector("#limit-warning")
 
 // get info from github
 var getRepoIssues = function(repo) {
@@ -8,6 +9,9 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if (response.headers.get("link")) {
+                    displayWarning(repo);
+                };
             });
         }
         else {
@@ -18,6 +22,10 @@ var getRepoIssues = function(repo) {
 
 // create display for issues
 var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent ="This repo has no open issues!";
+        return;
+    }
     for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
@@ -40,6 +48,18 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+// warning if there are more than 30 issues
+var displayWarning = function(repo) {
+    limitWarningEL.textContent = "To see more than 30 issues visit :"
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issuse on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank")
+
+    limitWarningEL.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
